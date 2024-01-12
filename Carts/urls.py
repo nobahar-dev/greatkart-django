a5c722d99f6176f654ad11ca2c3bdfcdcb1ccdaa
mahdi nobahar <nobahar.mahdi@yahoo.com>
2024-01-12 12:12:41 +0330

    1/12/2024

--- Carts/urls.py
+++ Carts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from Carts import views
+from . import views
 
 urlpatterns = [
-    path('', views.cart, name='cart')
+    path('', views.cart, name='cart'),
+    path('add_cart/<int:product_id>', views.add_cart, name='add_cart')
 ]
--- Carts/views.py
+++ Carts/views.py
@@ -1,5 +1,60 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+
+from Carts.models import Cart, CartItem
+from Store.models import Product
 
 
-def cart(request):
-    return render(request, 'store/carts.html')
+def _cart_id(request: HttpRequest):
+    my_cart_id = request.session.session_key
+
+    if not my_cart_id:
+        my_cart_id = request.session.create()
+
+    return my_cart_id
+
+
+def add_cart(request: HttpRequest, product_id):
+    product: Product = Product.objects.get(id=product_id)
+
+    try:
+        my_cart: Cart = Cart.objects.get(cart_id=_cart_id(request))
+
+    except Cart.DoesNotExist:
+        my_cart: Cart = Cart.objects.create(cart_id=_cart_id(request))
+        my_cart.save()
+
+    try:
+        cart_item: CartItem = CartItem.objects.get(product=product, cart=my_cart)
+        cart_item.quantity += 1
+        cart_item.save()
+
+    except CartItem.DoesNotExist:
+        cart_item: CartItem = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=my_cart
+        )
+        cart_item.save()
+    return redirect('cart')
+
+
+def cart(request, total_price=0, quantity=0, cart_items=None):
+    try:
+        my_cart: Cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items: CartItem = CartItem.objects.filter(cart=my_cart, is_active=True)
+
+        for cart_item in cart_items:
+            total_price += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity
+
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total_price': total_price,
+        'quantity': quantity,
+        'cart_items': cart_items
+    }
+    return render(request, 'store/carts.html', context)
